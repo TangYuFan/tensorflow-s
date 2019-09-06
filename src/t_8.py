f839244afead379c9424c83ef4dac4fdf9079653
995435702 <995435702@qq.com>
2019-09-06 11:35:29 +0800

    googleInceptionNet

---
+++ src/t_8.py
@@ -0,0 +1,10 @@
+'''
+ * @desc : tensorflow实现 googleInceptionNet
+ * @auth : TYF
+ * @date : 2019/8/31 - 15:58
+'''
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow.examples.tutorials.mnist.input_data as input_data
+from time import time
